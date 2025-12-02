745fa854416f3932f5da023d447fe1de8218228d
Jes1985 <votre-email@example.com>
2025-12-02 14:31:08 +0100

    Chore: force dynamic rendering for locale layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -6,6 +6,8 @@ import { locales } from '@/i18n'
 import { Header } from '@/components/layout/Header'
 import { Footer } from '@/components/layout/Footer'
 
+export const dynamic = 'force-dynamic'
+
 export function generateStaticParams() {
   return locales.map((locale) => ({ locale }))
 }
